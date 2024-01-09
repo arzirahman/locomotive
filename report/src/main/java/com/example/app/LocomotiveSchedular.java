7cf1080e43ae7736083296e9d01fdfec1d100d0f
Arzi Rahman <arzirahman07@gmail.com>
2024-01-09 16:26:59 +0700

    add telegra bot

--- report/src/main/java/com/example/app/LocomotiveSchedular.java
+++ report/src/main/java/com/example/app/LocomotiveSchedular.java
@@ -14,42 +14,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LocomotiveSchedular {
 
-    @Autowired
-    private LocomotiveRepository locomotiveRepository;
+        @Autowired
+        private LocomotiveRepository locomotiveRepository;
 
-    @Autowired
-    private LocomotiveSummaryRepository locomotiveSummaryRepository;
+        @Autowired
+        private LocomotiveSummaryRepository locomotiveSummaryRepository;
 
-    @Autowired
-    private TelegramBot telegramBot;
+        @Autowired
+        private TelegramBot telegramBot;
 
-    @Scheduled(fixedDelay = 10000)
-    public void scheduler() {
-        locomotiveSummaryRepository.deleteAll();
+        @Scheduled(fixedDelay = 10000)
+        public void scheduler() {
+                locomotiveSummaryRepository.deleteAll();
 
-        int totalLocomotif = (int) locomotiveRepository.count();
-        int totalPoorLocomotif = (int) locomotiveRepository.countByStatus("Poor");
-        int totalGoodLocomotif = (int) locomotiveRepository.countByStatus("Good");
-        int totalExcellentLocomotif = (int) locomotiveRepository.countByStatus("Excellent");
+                int totalLocomotif = (int) locomotiveRepository.count();
+                int totalPoorLocomotif = (int) locomotiveRepository.countByStatus("Poor");
+                int totalGoodLocomotif = (int) locomotiveRepository.countByStatus("Good");
+                int totalExcellentLocomotif = (int) locomotiveRepository.countByStatus("Excellent");
 
-        LocomotiveSummary locomotifSummary = LocomotiveSummary.builder()
-                .totalLocomotif(totalLocomotif)
-                .totalLocomotifPoor(totalPoorLocomotif)
-                .totalLocomotifGood(totalGoodLocomotif)
-                .totalLocomotifExcellent(totalExcellentLocomotif)
-                .build();
+                LocomotiveSummary locomotifSummary = LocomotiveSummary.builder()
+                                .totalLocomotif(totalLocomotif)
+                                .totalLocomotifPoor(totalPoorLocomotif)
+                                .totalLocomotifGood(totalGoodLocomotif)
+                                .totalLocomotifExcellent(totalExcellentLocomotif)
+                                .build();
 
-        locomotiveSummaryRepository.save(locomotifSummary);
+                locomotiveSummaryRepository.save(locomotifSummary);
 
-        log.info("Update Summary");
-        telegramBot.sendTextMessage("5767986311",
-                String.format("""
-                        <====> Locomtif Summary <====>
+                log.info("Update Summary");
+                telegramBot.sendTextMessage("5767986311",
+                                String.format("""
+                                                <====> Locomtif Summary <====>
 
-                        Total Locomotif = %d
-                        Total Poor Locomotif = %d
-                        Total Good Locomotif = %d
-                        Total Excellent Locomotif = %d
-                        """, totalLocomotif, totalPoorLocomotif, totalGoodLocomotif, totalExcellentLocomotif));
-    }
+                                                Total Locomotif = %d
+                                                Total Poor Locomotif = %d
+                                                Total Good Locomotif = %d
+                                                Total Excellent Locomotif = %d
+                                                """, totalLocomotif, totalPoorLocomotif, totalGoodLocomotif,
+                                                totalExcellentLocomotif));
+        }
 }
--- report/src/main/java/com/example/report/ReportApplication.java
+++ report/src/main/java/com/example/report/ReportApplication.java
@@ -1,13 +1,37 @@
 package com.example.report;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@SpringBootApplication
-public class ReportApplication {
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableMongoRepositories(basePackages = "com.example.repository.mongodb")
+@EnableJpaRepositories(basePackages = "com.example.repository.postgres")
+@EntityScan(basePackages = { "com.example.model" })
+public class ReportApplication implements ApplicationContextAware {
+	private static ApplicationContext ctx;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BeansException, TelegramApiException {
 		SpringApplication.run(ReportApplication.class, args);
+
+		TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+		botsApi.registerBot(ctx.getBean("telegramBot", AbilityBot.class));
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		ctx = applicationContext;
 	}
 
 }
